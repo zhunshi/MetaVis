d98ac89fc8543e4d626ff46ba61e976a22631d27
shizhun <shizhun@genomics.cn>
2022-04-07 10:48:08 +0800

    update src

---
+++ R/Themes.R
@@ -0,0 +1,11 @@
+mytheme <- ggplot2::theme(
+  axis.title = ggplot2::element_text(size = 14,color = "black"),
+  axis.text = ggplot2::element_text(size = 13,color = "black"),
+  # axis.text.x = ggplot2::element_text(angle = 45,hjust = 1,vjust = 1,face = "italic"),
+  legend.title = ggplot2::element_text(size = 14,color = "black"),
+  legend.text = ggplot2::element_text(size = 13,color = "black"),
+  panel.grid = ggplot2::element_blank(),
+  plot.title = ggplot2::element_text(size = 14,hjust = 0.5),
+  strip.text = ggplot2::element_text(size = 13),
+  strip.background = ggplot2::element_blank()
+)
